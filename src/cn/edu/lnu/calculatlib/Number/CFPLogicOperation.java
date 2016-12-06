3bdc169f0ac4d296391b0c1f4b9c856507f7d85d
yuanxiaokun <yuanxiaokun@outlook.com>
2016-12-06 18:24:11 +0800

    完善新增异常的注释。

--- src/cn/edu/lnu/calculatlib/Number/CFPLogicOperation.java
+++ src/cn/edu/lnu/calculatlib/Number/CFPLogicOperation.java
@@ -15,6 +15,7 @@ public interface CFPLogicOperation<T> {
      * 取模运算，置this的值为this%a，并返回this%a的值。
      * @param a 要取模的值
      * @throws CFPDivZeroExceptiion 当除数为0时抛出此异常
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return this%a的值
      */
     T mod(T a) throws CFPDivZeroExceptiion, CFPNonsupportedOperation;
@@ -22,6 +23,7 @@ public interface CFPLogicOperation<T> {
     /**
      * 或运算，置this的值为this|a，并返回this|a的值。
      * @param a 要进行或运算的值
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return this|a的值
      */
     T or(T a) throws CFPNonsupportedOperation;
@@ -29,6 +31,7 @@ public interface CFPLogicOperation<T> {
     /**
      * 异或运算，置this的值为this^a，并返回this^a的值。
      * @param a 要进行异或运算的值
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return this^a的值
      */
     T xor(T a) throws CFPNonsupportedOperation;
@@ -36,12 +39,14 @@ public interface CFPLogicOperation<T> {
     /**
      * 与运算，置this的值为this&a，并返回this&a的值。
      * @param a 要进行与运算的值
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return this&a的值
      */
     T and(T a) throws CFPNonsupportedOperation;
 
     /**
      * 非运算，置this的值为~this，并返回~this的值。
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return ~this的值
      */
     T not() throws CFPNonsupportedOperation;
@@ -49,6 +54,7 @@ public interface CFPLogicOperation<T> {
     /**
      * 循环左移运算，置this的值为this循环左移a位的值，并返回。
      * @param a 要进行循环移位运算的值
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return this循环左移a位的值
      */
     T lsl(T a) throws CFPNonsupportedOperation;
@@ -56,6 +62,7 @@ public interface CFPLogicOperation<T> {
     /**
      * 循环右移运算，置this的值为this循环右移a位的值，并返回。
      * @param a 要进行循环移位运算的值
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return this循环右移a位的值
      */
     T lsr(T a) throws CFPNonsupportedOperation;
@@ -63,6 +70,7 @@ public interface CFPLogicOperation<T> {
     /**
      * 左移运算，置this的值为this左移a位的值，并返回。
      * @param a 要进行移位运算的值
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return this左移a位的值
      */
     T sl(T a) throws CFPNonsupportedOperation;
@@ -70,6 +78,7 @@ public interface CFPLogicOperation<T> {
     /**
      * 右移运算，置this的值为this右移a位的值，并返回。
      * @param a 要进行移位运算的值
+     * @throws CFPNonsupportedOperation 当不支持该操作的数据类型调用该操作时抛出此异常
      * @return this右移a位的值
      */
     T sr(T a) throws CFPNonsupportedOperation;
